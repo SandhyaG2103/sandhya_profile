548f45000ed42d0375d126d9515ca1cec5bf2743
SandhyaG2103 <sandhyagaikwad352@gmail.com>
2024-08-09 13:17:10 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,150 @@
+import myimg from './assets/images/sandhya.jpg'
+const logotext = "Sandhya";
+const meta = {
+    title: "Sandhya",
+    description: "I am Sandhya Gaikwad, a MERN Stack Developer skilled in using MongoDB, Express.js, React, and Node.js to build full-stack web applications. I create dynamic user interfaces with React, develop server-side functionality with Node.js and Express.js, and manage data with MongoDB. My focus is on integrating these technologies to deliver efficient and scalable web solutions.",
+
+
+};
+
+const introdata = {
+    title: "I’m Sandhya",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop Web Application",
+    },
+    description: "I am Sandhya Gaikwad, a MERN Stack Developer specializing in MongoDB, Express.js, React, and Node.js. I build dynamic web applications with seamless integration of frontend and backend technologies.",
+
+    your_img_url: myimg ,
+};
+
+const dataabout = {
+    title: " about my self",
+    aboutme: "a passionate MERN stack developer with a Bachelor's in Computer Science from Nanasaheb Mahadik College. I have experience with MongoDB, Express.js, React, and Node.js, building dynamic and scalable web applications. During my internship at Growmore Technology, I worked on projects like a quiz application, a chat app, and an e-commerce website. I also developed a speech-to-text conversion tool and a PDF creation feature. I'm eager to apply my skills in creating innovative web solutions and continuously learning new technologies."
+
+};
+const worktimeline = [{
+        projectName: "Conversation of speech to text and create pdf",
+
+       
+       
+    },
+    {
+        projectName: "Quiz Application",
+
+        
+      
+    },
+    {
+        projectName: "E-commerce Website",
+     
+        
+    },
+];
+
+const skills = [{
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "Html",
+        value: 100,
+    },
+    {
+        name: "Css",
+        value: 100,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 85,
+    },
+    {
+        name: "Mongodb",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "E-commerce Website",
+
+        description: "ality and ensuring a seamless user experience across various devices.",
+    },
+    {
+        title: "Conversation of speech to text and create pdf  using react js",
+
+        description: "Developed a web application using React.",
+    },
+    {
+        title: "Quiz Application",
+
+        description: "I developed a dynamic quiz application using HTML, CSS, and JavaScript, designed to offer an engaging and interactive user experience. The application presents users with multiple-choice questions, providing instant feedback on their answers and tracking their scores throughout the quiz. It features a responsive design to ensure usability across various devices and screen sizes, and includes a timer function to add an element of challenge to each quiz. This project highlights my ability to create user-friendly applications with interactive elements and a focus on responsive design.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://cxotoday.com/wp-content/uploads/2023/02/ecommerce.png",
+        description: "I created an e-commerce website with secure user authentication, product listings, a shopping cart, and payment gateway integration.",
+        link: "#",
+    },
+    {
+        img: "https://www.shutterstock.com/image-vector/voice-search-optimization-flat-vector-260nw-1771974728.jpg",
+        description: "I developed a React.js application that converts speech to text and generates PDF documents from the transcribed content.",
+        link: "#",
+    },
+    {
+        img: "https://files.realpython.com/media/Build-a-Quiz-Application_Watermarked.373436013115.jpg",
+        description: "The quiz application is used for educational assessments, employee training, skill development, entertainment, and market research.",
+       
+    },
+    {
+        img: "https://miro.medium.com/v2/resize:fit:1400/1*TCzXTeJBL_FmtptpdSd8Ag.png",
+        description: "I created a user-friendly registration form for an e-commerce website with secure authentication using HTML and CSS.",
+      
+    },
+    {
+        img: "https://i.ytimg.com/vi/Ayx9jmR5Mt8/maxresdefault.jpg",
+        description: "I developed a secure cloud-based file transfer system using end-to-end encryption and AWS S3 for storage.",
+       
+    },
+       {
+        img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRDya7r7TYNr5G7mKYOTrc6GCkNVWaeg9MsA&s",
+        description:"An e-commerce website features user authentication, product listings, a shopping cart, and payment gateway integration to provide a secure and efficient online shopping experience."
+       }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "sandhyagaikwad352@gmail.com",
+    YOUR_FONE: "9665463880",
+    description: "I am a B.Tech graduate in Computer Science and Engineering based in Pune. Feel free to reach out if you have any questions about my projects, potential opportunities, or if you just want to connect. I look forward to hearing from you!.",
+
+
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/SandhyaG2103",
+    
+    linkedin: "https://www.linkedin.com/in/sandhya-gaikwad-953341222?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
